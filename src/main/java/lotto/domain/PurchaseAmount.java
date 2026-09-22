400a3ff49676ee5092d749c949431121c5e8f632
guhyung5645 <leeknivoc@naver.com>
2026-09-22 20:48:14 +0900

    feat: PurchaseAmount 구현

---
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -0,0 +1,14 @@
+package lotto.domain;
+
+public class PurchaseAmount {
+
+    public PurchaseAmount() {
+    }
+
+    public int calculateNumberOfLottos(int money) {
+        if (money < 1000) {
+            throw new IllegalArgumentException("구입 금액은 1000원 이상이어야 합니다.");
+        }
+        return money / 1000;
+    }
+}
