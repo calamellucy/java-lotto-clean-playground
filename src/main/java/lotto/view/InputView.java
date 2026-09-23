e3c4c8e56cbe93eedf9e8b6d3467486d84c1a9e4
guhyung5645 <leeknivoc@naver.com>
2026-09-23 19:43:22 +0900

    fix: nextInt-nextLine 혼용 버그 수정, 당첨번호 입력 기능 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,5 +1,7 @@
 package lotto.view;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
@@ -9,8 +11,25 @@ public class InputView {
     // 사용자에게 구입금액을 입력받는 메서드
     public static int inputMoney() {
         System.out.println("구입금액을 입력해 주세요.");
-        return scanner.nextInt();
+        return Integer.parseInt(scanner.nextLine());
     }
 
+    // 사용자에게 로또 번호를 입력받는 메서드
+    public static List<Integer> inputWinningNumbers() {
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        String input = scanner.nextLine();
+        return parseNumbers(input);
+    }
 
+    private static List<Integer> parseNumbers(String input) {
+        List<Integer> numbers = new ArrayList<>();
+        for (String number : input.split(",")) {
+            numbers.add(parseNumber(number));
+        }
+        return numbers;
+    }
+
+    private static int parseNumber(String number) {
+        return Integer.parseInt(number.trim());
+    }
 }
