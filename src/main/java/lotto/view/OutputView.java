1aba5dc3c36c64fcc6747b55a3290feb9f4d94de
guhyung5645 <leeknivoc@naver.com>
2026-09-23 19:43:45 +0900

    feat: OutputView에 당첨 통계 출력 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,6 +1,8 @@
 package lotto.view;
 
 import lotto.domain.Lottos;
+import lotto.domain.Rank;
+import lotto.domain.WinningStatistics;
 
 public class OutputView {
 
@@ -10,4 +12,26 @@ public class OutputView {
             System.out.println(lottos.getLottoList().get(i));
         }
     }
+
+    public static void printWinningStatistics(WinningStatistics statistics) {
+        System.out.println();
+        System.out.println("당첨 통계");
+        System.out.println("---------");
+        printRankResult(statistics, Rank.FOURTH);
+        printRankResult(statistics, Rank.THIRD);
+        printRankResult(statistics, Rank.SECOND);
+        printRankResult(statistics, Rank.FIRST);
+        printProfitRate(statistics);
+    }
+
+    private static void printRankResult(WinningStatistics statistics, Rank rank) {
+        int count = statistics.countRank(rank);
+        System.out.println(rank.getMatchCount() + "개 일치 (" + rank.getPrize() + "원)- " + count + "개");
+    }
+
+    private static void printProfitRate(WinningStatistics statistics) {
+        String profitRate = String.format("%.2f", statistics.getProfitRate());
+        System.out.println("총 수익률은 " + profitRate + "입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)");
+    }
+
 }
