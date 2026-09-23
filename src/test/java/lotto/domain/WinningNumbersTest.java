f6fbdce72f44ec1f00d5de9f60354a9f24c7cc63
guhyung5645 <leeknivoc@naver.com>
2026-09-23 19:45:38 +0900

    test: WinningNumbers 6개/중복 검증 테스트 추가

--- src/test/java/lotto/domain/WinningNumbersTest.java
+++ src/test/java/lotto/domain/WinningNumbersTest.java
@@ -1,4 +1,27 @@
 package lotto.domain;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class WinningNumbersTest {
+
+    @Test
+    @DisplayName("당첨 번호는 6개여야 한다")
+    void 당첨_번호는_6개여야_한다() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new WinningNumbers(List.of(1, 2, 3, 4, 5));
+        });
+    }
+
+    @Test
+    @DisplayName("당첨 번호는 중복될 수 없다")
+    void 당첨_번호는_중복될_수_없다() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new WinningNumbers(List.of(1, 2, 3, 4, 5, 5));
+        });
+    }
+
 }
