054dc55ba0311077c7df3d22c7fe4ddfaf0dc57f
guhyung5645 <leeknivoc@naver.com>
2026-09-23 19:44:00 +0900

    feat: LottoController에 당첨 확인 흐름 연결

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,8 +1,11 @@
 package lotto.controller;
 
+import java.util.List;
 import lotto.domain.LottoNumberGenerator;
 import lotto.domain.Lottos;
 import lotto.domain.PurchaseAmount;
+import lotto.domain.WinningNumbers;
+import lotto.domain.WinningStatistics;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -13,5 +16,10 @@ public class LottoController {
         int count = new PurchaseAmount().calculateNumberOfLottos(money);
         Lottos lottos = Lottos.generate(count, new LottoNumberGenerator());
         OutputView.printLottos(lottos);
+
+        List<Integer> winningNumberInput = InputView.inputWinningNumbers();
+        WinningNumbers winningNumbers = new WinningNumbers(winningNumberInput);
+        WinningStatistics statistics = new WinningStatistics(lottos, winningNumbers);
+        OutputView.printWinningStatistics(statistics);
     }
 }
