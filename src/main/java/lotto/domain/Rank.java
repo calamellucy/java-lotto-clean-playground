8ecd4015ab9c8ef88a44c6da01b7776da82cfe65
guhyung5645 <leeknivoc@naver.com>
2026-09-23 19:41:08 +0900

    feat: 등수와 상금을 매핑하는 Rank enum 추가

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,4 +1,26 @@
 package lotto.domain;
 
 public enum Rank {
+
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 1_500_000),
+    THIRD(4, 50_000),
+    FOURTH(3, 5_000),
+    MISS(0, 0);
+
+    private final int matchCount;
+    private final int prize;
+
+    Rank(int matchCount, int prize) {
+        this.matchCount = matchCount;
+        this.prize = prize;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
 }
